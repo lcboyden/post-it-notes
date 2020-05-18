8f338d81142d58800170b9276b7c90eaf77f4834
Lindsay Boyden <lindsayboyden@yahoo.com>
2020-05-18 15:47:28 -0600

    controller, routes, updating controller

---
+++ app/controllers/notes_controller.rb
@@ -0,0 +1,13 @@
+class NotesController < ApplicationController
+  def index
+    @notes = Note.all
+  end
+
+  def show
+    @notes = Note.find(params[:id])
+  end
+
+  def new
+    @notes = Note.new
+  end
+end
